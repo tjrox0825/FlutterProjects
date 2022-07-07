c7bc0340af356c1a5b6e46331e6cffc8bb336f48
Tarun <tarun.tanmay2510@gmail.com>
2022-07-07 14:51:15 +0530

    Day 3 - LoginPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'package:day2/pages/loginpage.dart';
 import 'package:flutter/material.dart';
-import 'package:day2/home_page.dart';
+import 'package:day2/pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +10,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
-    );
+        //home: HomePage(),
+        themeMode: ThemeMode.light,
+        theme: ThemeData(primarySwatch: Colors.deepPurple),
+        darkTheme: ThemeData(
+          brightness: Brightness.dark,
+        ),
+        initialRoute: "/home",
+        routes: {
+          "/":(context) => LoginPage(),
+          "/home": (context) => HomePage(),
+          "/login": (context) => LoginPage(),
+        });
   }
 }
